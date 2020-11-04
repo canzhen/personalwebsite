2300025cb852a2aaa6b9ee8356b6d205603301e0
Canzhen Zhou <canzhen@shift.com>
2020-11-03 21:41:44 -0800

    fix footer

--- src/client/src/components/Home/Footer/Footer.jsx
+++ src/client/src/components/Home/Footer/Footer.jsx
@@ -9,17 +9,15 @@ type FooterProps = {
 
 const Footer: React.FC<FooterProps> = props => {
   return (
-      <div className="footer">
-        <div className="fbar">
-            <p>
-              {props.language === 'EN' ? '@Copyright: Canzhen Zhou since 2020.' : 
-                props.language === 'CN' ? '©2020 Canzhen' : '' }
-            </p>
-            <Link className="fbarRight" to={props.language === 'EN' ? '/cn' : props.language === 'CN' ? '/' : ''}>
-              {props.language === 'EN' ? '中文' : 
-              props.language === 'CN' ? 'English' : ''}
-            </Link>
-        </div>
+      <div className="footer">  
+        <p>
+          {props.language === 'EN' ? '@Copyright: Canzhen Zhou since 2020.' : 
+            props.language === 'CN' ? '©2020 Canzhen' : '' }
+        </p>
+        <Link className="fbarRight" to={props.language === 'EN' ? '/cn' : props.language === 'CN' ? '/' : ''}>
+          {props.language === 'EN' ? '中文' : 
+          props.language === 'CN' ? 'English' : ''}
+        </Link>
       </div>
   );
 }
